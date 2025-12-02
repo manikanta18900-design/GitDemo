6931a70fe2fb8ec698aafb9ad9cd93e252c83f35
Mani <manikanta18900@gmail.com>
2025-12-02 13:35:41 +0530

    American guy Commit

--- src/test/java/com/framework/tests/SampleTest.java
+++ src/test/java/com/framework/tests/SampleTest.java
@@ -11,5 +11,10 @@ public class SampleTest extends BaseTest {
         driver.findElement(By.cssSelector("input[name='name']")).sendKeys("Mani");
         System.out.println("Test executed successfully1!");
         System.out.println("Test executed successfully2!");
-        System.out.println("Test executed successfully3!");    }
+        System.out.println("Test executed successfully3!"); 
+        
+        System.out.println("Test executed successfully3.1!");     
+        System.out.println("Test executed successfully3.2!"); 
+        System.out.println("Test executed successfully3.3!"); 
+    }
 }
